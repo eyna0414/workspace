ba4583dd8b718472f61698d5273916050f3d2618
eyna0414 <pey0414@gmail.com>
2025-03-17 18:09:03 +0900

    0317 test2

--- workspace_react/test2/src/components/ToDoList.jsx
+++ workspace_react/test2/src/components/ToDoList.jsx
@@ -1,26 +1,48 @@
 import React from 'react'
+import styles from './ToDoList.module.css'
+import icon_edit from '../assets/icons/edit.png'
+import icon_delete from '../assets/icons/delete.png'
+
 
 const ToDoList = () => {
   return (
     <>
-      <div>
-        <div>
+      <div className={styles.container}>
+        <div className={styles.firstContainer}>
           <h2>To Do List</h2>
         </div>
-        <div>
-          <input type="text" />
-          <button type='button'>등록</button>
-        </div>
-        <div>
-          <div>
+
+        <div className={styles.secondContainer}>
+          <div className={styles.insertContainer}>
             <input type="text" />
+            <button 
+              type='button'
+              className={styles.btn}
+            >등록</button>
           </div>
-          <div>
-            <input type="text" />
-            <button type='button'>확인</button>
-            <button type='button'>취소</button>
+  
+          <div className={styles.listContainer}>
+            <div >
+              <input type="text" />
+              <img src={icon_edit}/>
+              <img src={icon_delete}/>
+            </div>
+  
+            <div className={styles.lastContainer}>
+              <input type="text" />
+              <button 
+                type='button'
+                className={styles.btn}
+              >확인</button>
+              <button 
+                type='button'
+                className={styles.btn}
+              >취소</button>
+            </div>
           </div>
         </div>
+
+
       </div>
     </>
   )
