fa5a6c70b62dea7e0587a8f171fed296380d2582
eyna0414 <pey0414@gmail.com>
2025-03-18 17:31:27 +0900

    0318 test

--- workspace_react/test2/src/components/ToDoList.jsx
+++ workspace_react/test2/src/components/ToDoList.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './ToDoList.module.css'
 import icon_edit from '../assets/icons/edit.png'
 import icon_delete from '../assets/icons/delete.png'
+import { data } from '../../data/todoList'
 
 
 const ToDoList = () => {
+  const [todoList, settodoList] = useState(data);
+
+
   return (
     <>
       <div className={styles.container}>
@@ -12,34 +16,32 @@ const ToDoList = () => {
           <h2>To Do List</h2>
         </div>
 
+        <div className={styles.insertContainer}>
+          <input type="text" placeholder='+ Add a Task'/>
+          <button
+            type='button'
+            className={styles.btn}
+          >등록</button>
+        </div>
+
         <div className={styles.secondContainer}>
-          <div className={styles.insertContainer}>
+          <div className={styles.listContainer}>
+            <input type="text" />
+            <img src={icon_edit}/>
+            <img src={icon_delete}/>
+          </div>
+  
+          <div className={styles.lastContainer}>
             <input type="text" />
             <button 
               type='button'
               className={styles.btn}
-            >등록</button>
-          </div>
-  
-          <div className={styles.listContainer}>
-            <div >
-              <input type="text" />
-              <img src={icon_edit}/>
-              <img src={icon_delete}/>
-            </div>
-  
-            <div className={styles.lastContainer}>
-              <input type="text" />
-              <button 
-                type='button'
-                className={styles.btn}
-              >확인</button>
-              <button 
-                type='button'
-                className={styles.btn}
-              >취소</button>
-            </div>
-          </div>
+            >확인</button>
+            <button 
+              type='button'
+              className={styles.btn}
+            >취소</button>
+          </div>      
         </div>
 
 
